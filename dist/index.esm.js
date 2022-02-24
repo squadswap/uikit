93f061087bcfb2254879117d2d351976ca6dd28e
StardevX <workerkit001@gmail.com>
2022-02-24 20:26:59 +1000

    update social links

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -4048,65 +4048,12 @@ var socials = [
     {
         label: "Telegram",
         icon: "TelegramIcon",
-        // items: [
-        //   {
-        //     label: "English",
-        href: "https://t.me/GreenhouseSwap",
-        //   },
-        //   {
-        //     label: "Bahasa Indonesia",
-        //     href: "https://t.me/PancakeSwapIndonesia",
-        //   },
-        //   {
-        //     label: "中文",
-        //     href: "https://t.me/PancakeSwap_CN",
-        //   },
-        //   {
-        //     label: "Tiếng Việt",
-        //     href: "https://t.me/PancakeSwapVN",
-        //   },
-        //   {
-        //     label: "Italiano",
-        //     href: "https://t.me/pancakeswap_ita",
-        //   },
-        //   {
-        //     label: "русский",
-        //     href: "https://t.me/pancakeswap_ru",
-        //   },
-        //   {
-        //     label: "Türkiye",
-        //     href: "https://t.me/pancakeswapturkiye",
-        //   },
-        //   {
-        //     label: "Português",
-        //     href: "https://t.me/PancakeSwapPortuguese",
-        //   },
-        //   {
-        //     label: "Español",
-        //     href: "https://t.me/PancakeswapEs",
-        //   },
-        //   {
-        //     label: "日本語",
-        //     href: "https://t.me/pancakeswapjp",
-        //   },
-        //   {
-        //     label: "Français",
-        //     href: "https://t.me/pancakeswapfr",
-        //   },
-        //   {
-        //     label: "Announcements",
-        //     href: "https://t.me/PancakeSwapAnn",
-        //   },
-        //   {
-        //     label: "Whale Alert",
-        //     href: "https://t.me/PancakeSwapWhales",
-        //   },
-        // ],
+        href: "https://t.me/sqadtokenchat",
     },
     {
         label: "Twitter",
         icon: "TwitterIcon",
-        href: "https://twitter.com/GreenhouseSwap",
+        href: "https://mobile.twitter.com/sqadtoken",
     },
 ];
 var MENU_HEIGHT = 64;
